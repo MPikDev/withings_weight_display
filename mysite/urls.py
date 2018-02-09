3bd849963b8856e97c9f37a854c39ebb61bf546f
Mark Pikulik <markpikulik@Marks-MacBook-Pro.local>
2018-02-08 21:51:07 -0800

    got a login page to show if you go to /withings_page/ next step to get CSRF cookie set

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,6 +4,8 @@ from withings_data import views
 
 urlpatterns = patterns('',
 	# url(r'^accounts/login/', views.login, name='login'),
+	url('^', include('django.contrib.auth.urls')),
+	url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
 	url(r'^withings_page/', views.withings_page),
 	url(r'^get_data/', views.get_data),
 	url(r'^admin/', include(admin.site.urls)),
--- withings_data/views.py
+++ withings_data/views.py
@@ -1,11 +1,43 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from models import User
+from django.contrib.auth import authenticate, login
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
 
 
+def login(request):
+	username = request.POST['username']
+	password = request.POST['password']
+	user = authenticate(username=username, password=password)
+	print 'user , authenticate ', user
+	if not request.user.is_authenticated():
+		return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
+	if user is not None:
+		if user.is_active:
+			login(request, user)
+			return HttpResponse("Sucessful", status=200)
+		else:
+			return render(request, 'withings_data/get_data.html')
+
+	else:
+		# Return an 'invalid login' error message.
+		return HttpResponse("Sucessful", status=200)
+
+
+
+
+
+
+def logout_view(request):
+	logout(request)
+	# Redirect to a success page.
+
+@login_required
 def withings_page(request):
 	return render(request, 'withings_data/get_data.html')
 
+@login_required
 def get_data(request):
 	email = request.POST.get("email", "")
 	password = request.POST.get("password", "")
